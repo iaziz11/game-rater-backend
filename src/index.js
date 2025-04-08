4bebbd3a74945d579116cdb18cad099b0f574167
Imran Aziz <imrannoah11@gmail.com>
2025-04-07 21:04:39 -0700

    Fixed search functionality

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import { getCredits, getGamesFromQuery } from "./services/apiService.js";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+let access_token = "";
+let expire_time = 0;
+
+app.use(express.json());
+app.use(cors());
+app.use(async (req, res, next) => {
+  if (access_token == "" || Date.now() >= expire_time) {
+    try {
+      const data = await getCredits();
+      access_token = data.access_token;
+      expire_time = Date.now() + data.expires_in;
+      next();
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    next();
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  res.status(500).json({ message: err.message || "Internal Server Error" });
+});
+
+// Basic route
+app.get("/", (req, res) => {
+  return res.send("Hello, Node.js backend!");
+});
+
+app.get("/search", async (req, res) => {
+  let query = req.query["q"];
+  if (!query) {
+    return res.send("Please send valid query");
+  }
+  try {
+    const games = await getGamesFromQuery(query, access_token);
+    return res.json(games);
+  } catch (error) {
+    return res.status(500).json({ message: "Server Error", error: error });
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ src/services/apiService.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+const id = process.env.TWITCH_API_CLIENT_ID;
+const secret = process.env.TWITCH_API_CLIENT_SECRET;
+
+export async function getCredits() {
+  try {
+    const response = await axios.post("https://id.twitch.tv/oauth2/token", {
+      client_id: id,
+      client_secret: secret,
+      grant_type: "client_credentials",
+    });
+    return response.data;
+  } catch (error) {
+    return { error: error.cause };
+  }
+}
+
+export async function getGamesFromQuery(query, access_token) {
+  console.log(access_token);
+  console.log(id);
+  try {
+    const response = await axios.post(
+      "https://api.igdb.com/v4/games",
+      `search "${query}"; fields name, summary;`,
+      {
+        headers: {
+          Accept: "application/json",
+          "Client-ID": id,
+          Authorization: `Bearer ${access_token}`,
+          "Content-Type": "text/plain",
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
